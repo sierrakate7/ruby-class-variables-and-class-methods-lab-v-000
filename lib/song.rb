7de6cdbf3d42694a8b211e31c781889dc7591155
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-19 03:09:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,49 +1,35 @@
-class Song 
-  attr_accessor :name, :artist, :genre
-   attr_reader :artist
+class Song
+  attr_accessor :name, :artists, :genre
+  attr_reader :artist
   
-  @@count = 0
-  @@artists = []
+   @@count = 0
   @@genres = []
+  @@artists = []
   @@genre_count = {}
   @@artist_count = {}
-  
-  def self.count
-    @@count
-  end 
-  
-  def self.artists
-    @@artists
+   def initialize(name, artist, genre)
+    @@count += 1
+    @@genres << genre
+    @artist = artist
+    @@artists << artist
+    @name = name
+    @genre = genre
   end
-  
-  def self.genre 
-    @@genre
-  end 
-  
-  def initialize(name,artist,genre)
-  @@count += 1
-  @@artists << artist 
-  @@genre 
-  @name = name 
-  @artist = artist
-  @genre = genre
-  end 
-  
+   def self.count
+    @@count
+  end
    def self.artists
     @@artists.uniq
   end
-  
    def self.genres
     @@genres.uniq
   end
-  
    def self.genre_count
     @@genres.each do |genre|
       @@genre_count[genre] = @@genres.count(genre)
     end
     @@genre_count
   end
-  
    def self.artist_count
     @@artists.each do |artist|
       @@artist_count[artist] = @@artists.count(artist)
@@ -51,7 +37,3 @@ class Song
     @@artist_count
   end
 end
-  
-  
-
-  
