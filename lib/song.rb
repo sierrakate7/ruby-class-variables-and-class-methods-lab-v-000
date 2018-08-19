7a192f7b386f9b913cc543c0c2fb8b7f156b978f
Sierra McCahon <smitche4@mail.umw.edu>
2018-08-19 03:06:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,25 @@ class Song
   @genre = genre
   end 
   
+   def self.artists
+    @@artists.uniq
+  end
+   def self.genres
+    @@genres.uniq
+  end
+   def self.genre_count
+    @@genres.each do |genre|
+      @@genre_count[genre] = @@genres.count(genre)
+    end
+    @@genre_count
+  end
+   def self.artist_count
+    @@artists.each do |artist|
+      @@artist_count[artist] = @@artists.count(artist)
+    end
+    @@artist_count
+  end
+end
   
   
   
